8e61337f4e0c878f7948ce75f91c0299cf987909
Frederick Ernest <frederick.ernest@gmail.com>
2020-06-09 12:07:49 -0700

    update bussiness logic to split user input string into array
    co-authored-by: Emma Hellman<ehh801@icloud.comga

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,25 @@
 //Bussiness Logic
 const wordPuzzle = "puzzle";
-const wordSplit = wordPuzzle.split();
-console.log(wordSplit)
+const wordSplit = wordPuzzle.split('');
+console.log(wordSplit);
+vowels = [a, e, i, o, u];
 
+wordSplit.forEach(letter){
+  if (letter === vowels []) {
+    letter = "-"
+
+  }
+}
 
 //UI Logic
-
+for (let index= 0; index += 1)
 $("form#wordPuzzle").submit(function(event) {
 
+if (letter = 'a' || letter = 'e' |)
 
-  
 
+$().hide();
+$().show();
 
   event.preventDefault();
 
